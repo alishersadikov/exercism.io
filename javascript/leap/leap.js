5592345697f0047379bc81f9cbbdc1ba4253a3ab
Alisher Sadikov <alishersadikov@gmail.com>
2017-01-01 19:05:29 -0700

    refactor javascript/leap

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -3,15 +3,7 @@ var Year = function(year) {
 };
 
 Year.prototype.isLeap = function(){
-  if (this.year % 400 === 0) {
-    return true;
-  } else if (this.year % 100 === 0) {
-    return false;
-  } else if (this.year % 4 === 0) {
-    return true;
-  } else {
-    return false;
-  }
+  return this.year % 4 == 0 && (this.year % 100 != 0 || this.year % 400 == 0);
 };
 
 module.exports = Year;
