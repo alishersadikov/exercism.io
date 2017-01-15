0658181871392cd2a9a172f4ad075072b9203a44
Alisher Sadikov <alishersadikov@gmail.com>
2017-01-15 14:45:21 -0600

    complete javascript bob

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -5,19 +5,53 @@
 
 var Bob = function() {};
 
+
 Bob.prototype.hey = function(input) {
-  if (/[0-9]/.test(input) && !/[a-z]/i.test(input)) {
-    "Whatever."
-  }
-  else if (input  === input.toUpperCase()) {
-    return "Whoa, chill out!";
-  }
-  else if (input.charAt(input.length - 1) === "?") {
-    return "Sure."
+  var silent = input.trim() === '',
+      shouting = input.toUpperCase() === input && !silent,
+      allNumbers = input.search(/(\d+,?\s?)+[\.!\?]*/) != -1;
+
+  if (shouting && !allNumbers) {
+      return 'Whoa, chill out!';
+  } else if (input.endsWith("?")) {
+      return 'Sure.';
+  } else if (allNumbers) {
+      if (input.endsWith("!")) {
+          return 'Whoa, chill out!';
+      } else {
+          return "Whatever.";
+      }
+  } else if (silent) {
+      return "Fine. Be that way!";
   }
   else {
-    return "Whatever.";
+    return "Whatever."
   }
 };
 
 module.exports = Bob;
+
+// var response = "Whatever.",     // default response
+//     lastChar = input.charAt(input.length - 1),
+//     isSilence = input.trim() === '',
+//     isShouting = input.toUpperCase() === input && !isSilence,
+//     isAllNumbers = input.search(/(\d+,?\s?)+[\.!\?]*/) != -1,
+//     isQuestion = lastChar === '?',
+//     isExcl = lastChar === '!';
+//
+// if (isShouting && !isAllNumbers) {
+//     response = 'Whoa, chill out!';
+// } else if (isQuestion) {
+//     response = 'Sure.';
+// } else if (isAllNumbers) {
+//     if (isExcl) {
+//         response = 'Whoa, chill out!';
+//     } else {
+//         response = "Whatever.";
+//     }
+// } else if (isSilence) {
+//     response = "Fine. Be that way!";
+// }
+//
+// return response;
+// };
